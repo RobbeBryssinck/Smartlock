50241aeecd8b56bc0c91a5162a22e314d8a30759
Robbe Bryssinck <robbebryssinck@gmail.com>
2019-12-09 21:25:48 +0100

    Initial mobile app commit

---
+++ SmartlockApp/SmartlockApp/SmartlockApp/MainPage.xaml.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+using System.Net;
+using System.Net.Sockets;
+
+namespace SmartlockApp
+{
+    // Learn more about making custom code visible in the Xamarin.Forms previewer
+    // by visiting https://aka.ms/xamarinforms-previewer
+    [DesignTimeVisible(false)]
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        public void OnClickConnect(object sender, EventArgs e)
+        {
+            Socket sock = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
+
+            IPEndPoint serverEP = new IPEndPoint(IPAddress.Parse("145.93.88.229"), 10000);
+            sock.Connect(serverEP);
+            EndPoint remote = (EndPoint)(serverEP);
+
+            string message = "CLIENT LOGIN " + usernameEntry.Text + " " + passwordEntry.Text;
+            byte[] msgBuffer = Encoding.ASCII.GetBytes(message);
+            sock.Send(msgBuffer);
+            
+            byte[] recvBuffer = new byte[1024];
+            int recv = sock.ReceiveFrom(recvBuffer, ref remote);
+            message = Encoding.ASCII.GetString(recvBuffer, 0, recv);
+            if (message == "INVALID LOGIN")
+            {
+                sock.Close();
+                loginLabel.Text = "Login status: invalid login";
+            }
+            else
+            {
+                loginLabel.Text = "Login status: valid login";
+                // open activity
+            }
+        }
+    }
+}
