086627ab4ab63297e3ba97af8bac834d3ec3d6f5
Robbe Bryssinck <robbebryssinck@gmail.com>
2020-01-06 10:45:47 +0100

    Minor bugfixes

--- lock.py
+++ lock.py
@@ -6,7 +6,7 @@ Usage: set lock IP-address to the machine's IP.
 import socket
 import sys
 
-IP = '192.168.0.102'
+IP = '145.93.89.25'
 lock_state = 'LOCKED'
 
 sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
